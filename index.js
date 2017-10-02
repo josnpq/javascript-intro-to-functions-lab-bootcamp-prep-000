06676999d5b6e9f1219dad1e3b14eeea7c43e4ff
jose pardo <josn.pq@gmail.com>
2017-10-02 06:34:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ logWhisper("hello")
 
 function sayHiToGrandma(string) {
   if (string.toLowerCase === string){
-    return("I can't hear you")
+    return("I can\'t hear you")
     }
    if (string.toUpperCase === string) {
      return("YES INDEED!")
